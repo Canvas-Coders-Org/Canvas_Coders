e5ab0c2821f96e02e5ff3a4bc1965315593a6b1a
TechieTeee <TechieTeee@users.noreply.github.com>
2023-04-01 03:15:35 -0500

    Added Second Section, Flex box and Padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,17 +25,38 @@ const Home: NextPage = () => {
 
             <main className="flex-grow p-8 text-center">
                 <div className="max-w-screen-lg mx-auto">
-                    <h1 className="pb-8 text-4xl font-bold" style={{ fontFamily: "Anton", color: "white", fontSize: "800%" }}>CANVAS CODERS</h1>
+                    <h1 className="pb-8 text-4xl font-bold" style={{ fontFamily: "Anton", color: "white", fontSize: "500%" }}>CANVAS CODERS</h1>
                     <p className="mt-8 text-lg font-bold" style={{ color: "#FFDE59", fontSize: "200%" }}>
                         <strong>Unleash Your Creativity through Code</strong>
                     </p>
                     <div className="mt-8">
-                        <button style={{ backgroundColor: "#ccb49c", color: "white", fontWeight: "bold", padding: "0.5em 1em", borderRadius: "0.25em", cursor: "pointer" }}>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ backgroundColor: "#ccb49c", fontSize: "200%" }}>
                             Sign Up
                         </button>
                     </div>
                 </div>
             </main>
+
+            <section className="py-16 bg-gray-100 text-center">
+                <div className="max-w-screen-lg mx-auto">
+                    <h2 className="text-4xl font-bold mb-8">Personalized Learning Tracks</h2>
+                    <p className="text-xl font-light mb-16">Learning Tracks for Visual and Analytical Learners Alike</p>
+                    <div className="flex justify-center">
+                        <div className="flex flex-col items-center mr-4">
+                            <div className="w-80 h-80 relative">
+                                <img src="/artist_track.jpg" alt="Gallery Image 1" className="rounded-lg w-full h-full object-cover" />
+                            </div>
+                            <p className="text-lg font-bold mt-4">Artist Track</p>
+                        </div>
+                        <div className="flex flex-col items-center ml-4">
+                            <div className="w-80 h-80 relative">
+                                <img src="/developer_track.jpg" alt="Gallery Image 2" className="rounded-lg w-full h-full object-cover" />
+                            </div>
+                            <p className="text-lg font-bold mt-4">Developer Track</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             <footer className="p-4 mt-auto">
                 <div className="max-w-screen-lg mx-auto">
@@ -48,4 +69,4 @@ const Home: NextPage = () => {
     )
 }
 
-export default Home
+export default Home;
