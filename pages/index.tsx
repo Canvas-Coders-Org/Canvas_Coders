4ff9aa2e7714ce2cfa33fee9260d3e4200ea4a07
TechieTeee <TechieTeee@users.noreply.github.com>
2023-04-01 12:54:34 -0500

    Returned to earlier version of deployment to troubleshoot error

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Web3Button, Web3Address } from '../components/'
 
 const Home: NextPage = () => {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col h-screen" style={{ backgroundImage: "url('/Canvas_Coders_Background_Image.png')" }}>
             <Head>
                 <title>CANVAS CODERS</title>
                 <meta name="description" content="Canvas Coders Educational Coding and Art Platform" />
@@ -25,18 +25,59 @@ const Home: NextPage = () => {
 
             <main className="flex-grow p-8 text-center">
                 <div className="max-w-screen-lg mx-auto">
-                    <h1 className="pb-8 text-4xl font-bold" style={{ fontFamily: "Anton", color: "black", fontSize: "500%" }}>CANVAS CODERS</h1>
+                    <h1 className="pb-8 text-4xl font-bold" style={{ fontFamily: "Anton", color: "white", fontSize: "500%" }}>CANVAS CODERS</h1>
                     <p className="mt-8 text-lg font-bold" style={{ color: "#FFDE59", fontSize: "200%" }}>
                         <strong>Unleash Your Creativity through Code</strong>
                     </p>
                     <div className="mt-8">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ backgroundColor: "#ccb49c", fontSize: "200%" }}>
+                            Sign Up
+                        </button>
                     </div>
                 </div>
             </main>
 
-            <iframe src="https://codepen-app.vercel.app/codepen" style={{ width: "100%", height: "calc(100vh - 100px)", border: "none" }} />
+            <section className="py-16 bg-gray-100 text-center">
+                <div className="max-w-screen-lg mx-auto">
+                    <h2 className="text-4xl font-bold mb-8">Personalized Learning Tracks</h2>
+                    <p className="text-xl font-light mb-16">Learning Tracks for Visual and Analytical Learners Alike</p>
+                    <div className="flex justify-center">
+                        <div className="flex flex-col items-center mr-4">
+                            <div className="w-80 h-80 relative">
+                                <img src="/artist_track.jpg" alt="Gallery Image 1" className="rounded-lg w-full h-full object-cover" />
+                            </div>
+                            <p className="text-lg font-bold mt-4">Artist Track</p>
+                        </div>
+                        <div className="flex flex-col items-center ml-4">
+                            <div className="w-80 h-80 relative">
+                                <img src="/developer_track.jpg" alt="Gallery Image 2" className="rounded-lg w-full h-full object-cover" />
+                            </div>
+                            <p className="text-lg font-bold mt-4">Developer Track</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-            <footer className="p-4">
+            <section className="py-16 text-center" style={{
+                backgroundImage: "url('/pattern_background.jpg')",
+                backgroundSize: "cover",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <div className="max-w-screen-lg mx-auto">
+                    <h2 className="text-4xl font-bold mb-8 text-black">Experience our Platform</h2>
+                    <p className="text-xl font-light mb-16 text-black font-bold">Try it out!</p>
+                    <div className="mt-8">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ backgroundColor: "#ccb49c", fontSize: "200%" }}>
+                            Demo Now
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="p-4 mt-auto">
                 <div className="max-w-screen-lg mx-auto">
                     <p className="text-lg font-light">
                         CANVAS CODERS &copy; {new Date().getFullYear()}
